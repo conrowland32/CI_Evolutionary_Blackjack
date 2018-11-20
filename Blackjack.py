91ae9840ef9e3cfa6b2d503878790a5226068f4b
conrowland32 <conrowland32@gmail.com>
2018-11-20 07:53:15 -0600

    Add evolution

--- Blackjack.py
+++ Blackjack.py
@@ -1,18 +1,100 @@
 import sys
-import numpy as np
+import multiprocessing
+import time
+import random as rand
+from itertools import product
 from Player import Player
 from Shoe import Shoe
 
 
-POPULATION_SIZE = 60
+POPULATION_SIZE = 100
 NUMBER_DECKS = 6
-HANDS_PER_GENERATION = 1000
+HANDS_PER_GENERATION = 2500
 NUMBER_GENERATIONS = 1000
+MUTATION_RATE = 0.1
+
+
+def worker(decision_table=None, split_table=None):
+    shoe = Shoe(NUMBER_DECKS)
+    player = Player(decision_table, split_table)
+    finish_amount = player.play(shoe, HANDS_PER_GENERATION)
+    # print(finish_amount)
+    return finish_amount
+
+
+def mutate(population):
+    for i in range(0, len(population), 2):
+        individual = population[i]
+        for key in individual[0]:
+            for i, _ in enumerate(individual[0][key]):
+                if rand.random() < MUTATION_RATE:
+                    individual[0][key][i] = rand.choice(
+                        ["S", "H", "DS", "DH"])
+        for key in individual[1]:
+            for i, _ in enumerate(individual[1][key]):
+                if rand.random() < MUTATION_RATE:
+                    individual[1][key][i] = rand.choice(["P", "D"])
 
 
 if __name__ == "__main__":
-    shoe = Shoe(NUMBER_DECKS)
-    player1 = Player()
-    player1.play(shoe, HANDS_PER_GENERATION)
+    # Initialize starting decision tables
+    new_population = []
+    for _ in range(POPULATION_SIZE):
+        start_decision_table = {
+            "H4": [], "H5": [], "H6": [], "H7": [], "H8": [],
+            "H9": [], "H10": [], "H11": [], "H12": [], "H13": [],
+            "H14": [], "H15": [], "H16": [], "H17": [], "H18": [],
+            "H19": [], "H20": [], "S12": [], "S13": [], "S14": [],
+            "S15": [], "S16": [], "S17": [], "S18": [], "S19": [], "S20": []
+        }
+        start_split_table = {
+            "2": [], "3": [], "4": [], "5": [], "6": [],
+            "7": [], "8": [], "9": [], "10": [], "A": []
+        }
+
+        for hand in start_decision_table:
+            for _ in range(10):
+                start_decision_table[hand].append(
+                    rand.choice(["S", "H", "DS", "DH"]))
+
+        for hand in start_split_table:
+            for _ in range(10):
+                start_split_table[hand].append(rand.choice(["P", "D"]))
+
+        new_population.append((start_decision_table, start_split_table))
+
+    # Spawn and run processes for generation
+    with multiprocessing.Pool(POPULATION_SIZE) as pool:
+        fitness_scores = pool.starmap(worker, new_population)
+
+    test_table = new_population[0]
+    print('Generation 1:', str(sum(fitness_scores) / len(fitness_scores)))
+
+    # Run more generations
+    for gen in range(2, NUMBER_GENERATIONS+1):
+        # Get the top half of the population
+        survivors = sorted(fitness_scores, reverse=True)[
+            :int(POPULATION_SIZE/2)]
+        survivor_indices = []
+        for individual in survivors:
+            occurences = [i for i, x in enumerate(
+                fitness_scores) if x == individual]
+            if len(occurences) > 1:
+                for ind in occurences:
+                    if ind not in survivor_indices:
+                        survivor_indices.append(ind)
+            else:
+                survivor_indices.append(occurences[0])
+
+        # Create new population
+        new_population = [new_population[x]
+                          for x in survivor_indices for _ in range(2)]
+        mutate(new_population)
+
+        with multiprocessing.Pool(POPULATION_SIZE) as pool:
+            fitness_scores = pool.starmap(worker, new_population)
+        print('Generation ' + str(gen) + ': ' +
+              str(sum(fitness_scores) / len(fitness_scores)))
+        print(new_population)
 
     sys.exit()
--- Player.py
+++ Player.py
@@ -22,41 +22,38 @@ D  -> Default (to normal value)
 
 class Player:
     def __init__(self, decision_table=None, split_table=None):
-        if decision_table is None:
-            self.random_start()
-        else:
-            self.decision_table = decision_table
-            self.split_table = split_table
-        self.money = 0
+        self.decision_table = decision_table
+        self.split_table = split_table
+        self.money = 0.0
 
-    def random_start(self):
-        self.decision_table = {
-            "H4": [], "H5": [], "H6": [], "H7": [], "H8": [],
-            "H9": [], "H10": [], "H11": [], "H12": [], "H13": [],
-            "H14": [], "H15": [], "H16": [], "H17": [], "H18": [],
-            "H19": [], "H20": [], "S12": [], "S13": [], "S14": [],
-            "S15": [], "S16": [], "S17": [], "S18": [], "S19": [], "S20": []
-        }
-        self.split_table = {
-            "2": [], "3": [], "4": [], "5": [], "6": [],
-            "7": [], "8": [], "9": [], "10": [], "A": []
-        }
+    # def random_start(self):
+    #     self.decision_table = {
+    #         "H4": [], "H5": [], "H6": [], "H7": [], "H8": [],
+    #         "H9": [], "H10": [], "H11": [], "H12": [], "H13": [],
+    #         "H14": [], "H15": [], "H16": [], "H17": [], "H18": [],
+    #         "H19": [], "H20": [], "S12": [], "S13": [], "S14": [],
+    #         "S15": [], "S16": [], "S17": [], "S18": [], "S19": [], "S20": []
+    #     }
+    #     self.split_table = {
+    #         "2": [], "3": [], "4": [], "5": [], "6": [],
+    #         "7": [], "8": [], "9": [], "10": [], "A": []
+    #     }
 
-        for hand in self.decision_table:
-            for _ in range(10):
-                self.decision_table[hand].append(
-                    rand.choice(["S", "H", "DS", "DH"]))
-        # print(self.decision_table)
+    #     for hand in self.decision_table:
+    #         for _ in range(10):
+    #             self.decision_table[hand].append(
+    #                 rand.choice(["S", "H", "DS", "DH"]))
+    #     # print(self.decision_table)
 
-        for hand in self.split_table:
-            for _ in range(10):
-                self.split_table[hand].append(rand.choice(["P", "D"]))
-        # print(self.split_table)
+    #     for hand in self.split_table:
+    #         for _ in range(10):
+    #             self.split_table[hand].append(rand.choice(["P", "D"]))
+    #     # print(self.split_table)
 
     def play(self, shoe, num_hands):
         # Play number of hands for full generation
-        for i in range(num_hands):
-            print('Hand ', str(i))
+        for _ in range(num_hands):
+            # print('Hand ', str(i))
             # Reshuffle shoe every hand -- simulates CSM
             shoe.shuffle()
             new_cards = shoe.deal()
@@ -69,7 +66,7 @@ class Player:
             # Check for dealer blackjack
             if ch.get_raw_value(self.dealer_hand) == 21:
                 self.money -= BET_VALUE
-                print('Dealer BlackJack!', self.money)
+                # print('Dealer BlackJack!', self.money)
                 continue
 
             # Play the hand, and if it is split, play the split hand
@@ -89,26 +86,27 @@ class Player:
                 winner = ch.check_winner(self.hand1, self.dealer_hand)
                 if winner == 'player':
                     self.money += self.hand1_bet
-                    print('Player wins', self.money)
+                    # print('Player wins', self.money)
                 elif winner == 'dealer':
                     self.money -= self.hand1_bet
-                    print('Dealer wins', self.money)
+                    # print('Dealer wins', self.money)
             if self.hand2 is not None:
                 winner = ch.check_winner(self.hand2, self.dealer_hand)
                 if winner == 'player':
                     self.money += self.hand2_bet
-                    print('Player wins', self.money)
+                    # print('Player wins', self.money)
                 elif winner == 'dealer':
                     self.money -= self.hand2_bet
-                    print('Dealer wins', self.money)
+                    # print('Dealer wins', self.money)
             # print(self.hand1, self.hand2, self.dealer_hand)
+        return self.money
 
     def play_hand(self, shoe, hand):
         # Play hand using decision tables
         hand_val = ch.get_adjusted_value(hand)
         if hand_val == 21:
             self.money += 1.5 * BET_VALUE
-            print('Blackjack!', str(self.money))
+            # print('Blackjack!', str(self.money))
             return
         while hand_val < 21:
             decision = self.get_decision(hand_val, hand)
